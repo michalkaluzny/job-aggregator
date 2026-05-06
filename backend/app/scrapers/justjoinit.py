2938253cb0eb12c9852bfdb20aa4c8fb5788382f
michalkaluzny <mkaluznypraca@gmail.com>
2026-05-06 15:06:33 +0200

    feat(scrapers): use JobOffer model in justjoinit scraper

--- backend/app/scrapers/justjoinit.py
+++ backend/app/scrapers/justjoinit.py
@@ -1,7 +1,8 @@
 import httpx
+from app.models.job_offer import JobOffer
 
 BASE_URL = 'https://justjoin.it/api/candidate-api/offers'
-def fetch_offers(items_count : int = 10) -> list[dict]:
+def fetch_offers(items_count : int = 10) -> list[JobOffer]:
     response = httpx.get(BASE_URL, params={'itemsCount': items_count})
     if response.status_code == 200:
         try:
@@ -11,8 +12,9 @@ def fetch_offers(items_count : int = 10) -> list[dict]:
             if not offers:
                 print('No offers found')
                 return []
+            print(offers)
+            return [JobOffer.from_api(raw) for raw in offers]
 
-            return offers
 
         except Exception as e:
             print(e)
@@ -31,10 +33,17 @@ def main():
     first = offers[0]
     print(f"fetched {len(offers)} offers")
     print(f"First offer: ")
-    print(f"Title: {first['title']}")
-    print(f"Company: {first['companyName']}")
-    print(f"City: {first['city']}")
-    print(offers[0])
+    print(f"Title: {first.title}")
+    print(f"Company: {first.company_name}")
+    print(f"City: {first.locations[0].city}")
+    print(f"experience level: {first.experience_level}")
+
+    for i, offer in enumerate(offers, 1):
+
+        if offer.salary_from:
+            print(f'Salary for offer {i}: {offer.salary_from} - {offer.salary_to} : {offer.salary_currency}')
+        else:
+            print(f'No info about salary for offer {i}')
 
 if __name__ == '__main__':
     main()
