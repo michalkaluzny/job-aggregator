3d66c342d875752cf34322e138d1e9c3b348ddd2
michalkaluzny <mkaluznypraca@gmail.com>
2026-05-08 15:15:58 +0200

    feat(database): add creating database logic

---
+++ backend/app/database/init_db.py
@@ -0,0 +1,13 @@
+from app.database.db import engine, Base
+
+from app.models.job_offer_db import JobOfferDB, LocationDB
+
+
+def init_db():
+    """Create all tables in the database."""
+    Base.metadata.create_all(engine)
+    print("Database initialized successfully!")
+
+
+if __name__ == "__main__":
+    init_db()
